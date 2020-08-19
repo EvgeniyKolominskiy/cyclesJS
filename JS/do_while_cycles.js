cbc7cfd7bf676e41037bfac9f9d77ba3eb836d23
Evgeniy Kolominskiy <kolominskiyy@gmail.com>
2020-08-19 19:39:49 +0300

    Add tasks.

--- JS/do_while_cycles.js
+++ JS/do_while_cycles.js
@@ -1,2 +1,27 @@
 'use strict';
 
+/*
+ Задания, в которых необходимо использовать DO WHILE.
+ 1. Предлагать пользователю решить пример 2 + 2 * 2 до тех
+ пор, пока он не решит его правильно.
+ 2. Делить число 1000 на 2 до тех пор, пока не получится число
+ меньше 50. Вывести это число и сколько делений произвели.
+*/
+
+//Task 1
+
+let userInput = null;
+do {
+	userInput = Number (prompt(' Please work it out 2 + 2 * 2'));
+} while (userInput !== 2 + 2 * 2);
+
+//Task 2
+
+let n = 1000;
+let counter = 0;
+do {
+	n /= 2;
+	counter++;
+} while (n >= 50);
+
+console.log(`Result: ${n}, Number of divisions: ${counter}`);
--- JS/for_cycles.js
+++ JS/for_cycles.js
@@ -1,2 +1,17 @@
 'use strict';
 
+/*
+ Задания, в которых необходимо использовать FOR.
+ 1. Вывести все числа от 1 до 100, которые кратные указанному
+ пользователем числу.
+ */
+
+//Task 1
+
+const userNumber = Number(prompt('Please enter your number'));
+
+for(let i = 1; i <= 100; i++) {
+	if (i % userNumber === 0) {
+		console.log(i);
+	}
+}
